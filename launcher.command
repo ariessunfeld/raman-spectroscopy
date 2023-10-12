a373f8d0732ba4425970b4b33b98e9706793b6e4
Ari <ariessunfeld@gmail.com>
2023-10-12 19:10:06 -0400

    added launcher.command file and manifest/updater pair for auto updates

---
+++ launcher.command
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change directory to the script location
+cd $DIR
+
+# Check if virtual environment exists
+if [ ! -d "venv" ]; then
+  python3.11 -m venv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+else
+  source venv/bin/activate
+fi
+
+# Start the updater
+# python updater.py
+
+# After updates, run the main program from the latest version
+VERSION_FOLDER=$(ls | grep "v[0-9]\+\.[0-9]\+\.[0-9]\+$" | sort -V | tail -n 1)
+python $VERSION_FOLDER/gui.py
+
---
+++ updater.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from shutil import unpack_archive
+
+# Constants
+MANIFEST_URL = "https://raw.githubusercontent.com/your_username/your_repo/main/manifest.json"
+CURRENT_VERSION_FILE = "version.txt"
+
+def get_remote_version():
+    response = requests.get(MANIFEST_URL)
+    data = response.json()
+    return data['version'], data['download_url']
+
+def get_local_version():
+    with open(CURRENT_VERSION_FILE, 'r') as file:
+        return file.read().strip()
+
+def update_version(version):
+    with open(CURRENT_VERSION_FILE, 'w') as file:
+        file.write(version)
+
+def main():
+    local_version = get_local_version()
+    remote_version, download_url = get_remote_version()
+    
+    if local_version != remote_version:
+        print(f"New version {remote_version} is available. You have {local_version}.")
+        choice = input("Do you want to update? [Y/N]: ").lower()
+        
+        if choice == 'y':
+            # Download and extract new version
+            response = requests.get(download_url, stream=True)
+            with open("temp_update.zip", 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            unpack_archive("temp_update.zip", remote_version)
+            os.remove("temp_update.zip")
+            
+            # Update the version locally
+            update_version(remote_version)
+
+if __name__ == "__main__":
+    main()
+
