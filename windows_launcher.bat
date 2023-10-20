40173e8c191f362e2b54578beecd7d1a358c6b2f
Ari <ariessunfeld@gmail.com>
2023-10-20 13:51:51 +0200

    tested windows launcher and fixed bugs

--- windows_launcher.bat
+++ windows_launcher.bat
@@ -9,18 +9,21 @@ cd /d "%DIR%"
 
 :: Check if virtual environment exists
 IF NOT EXIST "venv" (
+    echo No virtual environment found; creating one...
     python3.11 -m venv venv
-    venv\Scripts\activate
+    call venv\Scripts\activate.bat
     pip install -r requirements.txt
 ) ELSE (
-    venv\Scripts\activate
+    echo Found a virtual environment; activating it...
+    call venv\Scripts\activate.bat
 )
 
 :: Start the updater
+echo Checking for updates...
 python updater.py
 
 :: After updates, run the main program from the latest version
-for /f "delims=" %%I in ('dir /b /ad /o-n "v*"') do (
+for /f %%I in ('powershell -command "Get-ChildItem -Directory 'v[0-9]*' | Sort-Object { [Version]($_.Name -replace 'v') } -Descending | Select-Object -First 1 -ExpandProperty Name"') do (
     SET "VERSION_FOLDER=%%I"
     goto :found
 )
@@ -31,7 +34,7 @@ exit /b
 
 :found
 cd "%VERSION_FOLDER%"
+echo Launching version %VERSION_FOLDER%...
 python gui.py
 
 ENDLOCAL
-
